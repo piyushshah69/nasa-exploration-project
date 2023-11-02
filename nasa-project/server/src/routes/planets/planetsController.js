f388062bc9be89e1454d8e87d62ef3edfd7b96fc
Piyush Kumar Shah <110243671+piyushshah69@users.noreply.github.com>
2023-11-02 20:51:14 +0530

    Delete nasa-project directory

--- nasa-project/server/src/routes/planets/planetsController.js
+++
@@ -1,9 +0,0 @@
-const { getAllPlanets } = require("../../models/planetsModel");
-
-function httpGetAllPlanets(req, res) {
-    return res.status(200).json(getAllPlanets());
-}
-
-module.exports = {
-    httpGetAllPlanets,
-};
--- nasa-project/server/src/routes/planets/planetsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { httpGetAllPlanets } = require('./planetsController');
-
-const planetsRouter = express.Router();
-
-planetsRouter.get('/', httpGetAllPlanets);
-
-module.exports = planetsRouter;
